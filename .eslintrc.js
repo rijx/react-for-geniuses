9fd8e56735a11238610f50839ffd91f37c1a0a7c
Mitchell de Rijcke <mdr@rijx.com>
2020-04-11 18:54:37 +0200

    Enforce consistent imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "babel-eslint",
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "import"],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2018,
@@ -21,5 +21,29 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "react/react-in-jsx-scope": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          "parent",
+          ["sibling", "index"],
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          caseInsensitive: true,
+          order: "asc",
+        },
+      },
+    ],
+    "import/no-self-import": "error",
+    "import/no-useless-path-segments": [
+      "error",
+      {
+        noUselessIndex: true,
+      },
+    ],
+    "import/newline-after-import": "error",
   },
 };
