821200d54301040913979718ac41601b38af7f10
Mitchell de Rijcke <mdr@rijx.com>
2020-04-11 17:40:20 +0200

    Configure ESLint to use ES2018 and CommonJS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   extends: ["eslint:recommended"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
     browser: true,
+    node: true,
   },
 };
