263354f71d302a327f65e49c998e42a1323bcefa
Mitchell de Rijcke <mdr@rijx.com>
2020-04-25 18:16:57 +0200

    Update test to work with the todo example

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
 const { shallow } = require("enzyme");
 const expect = require("expect");
 
+require.extensions[".scss"] = () => null;
+
 const App = require("./App");
 
 describe("<App />", () => {
-  it("renders a hello world title", () => {
+  it("renders a title", () => {
     const wrapper = shallow(<App />);
     const h1 = wrapper.find("h1");
     expect(h1.length).toBe(1);
-    expect(h1.text()).toBe("Hello world!");
+    expect(h1.text()).toBe("Todo list");
   });
 });
