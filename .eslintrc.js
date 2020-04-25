3d5dbbe8579b8bd126b592111530f782c6d87710
Mitchell de Rijcke <mdr@rijx.com>
2020-04-25 16:41:09 +0200

    Added self protecting import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,5 +45,28 @@ module.exports = {
       },
     ],
     "import/newline-after-import": "error",
+    "import/no-unresolved": ["error", { commonjs: true }],
+    "import/no-absolute-path": "error",
+    // The use of dynamic requires is a powerful feature of CommonJS.
+    // We want to discourage it whenever possible, but when its true
+    // power is warranted you'll have to use:
+    // require(someVar) // eslint-disable-line import/no-dynamic-require
+    "import/no-dynamic-require": "error",
+    "import/no-internal-modules": "error",
+    // Use the Webpack module rules instead of putting them in your
+    // require statements.
+    "import/no-webpack-loader-syntax": "error",
+    "import/no-self-import": "error",
+    "import/no-cycle": "error",
+    "import/no-useless-path-segments": "error",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: ["*.js", "src/**/*.test.js"],
+        optionalDependencies: true,
+        peerDependencies: true,
+        bundledDependencies: true,
+      },
+    ],
   },
 };
