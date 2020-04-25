8b652f7d2ab5afbd6f66406bf46d142ddc133909
Mitchell de Rijcke <mdr@rijx.com>
2020-04-25 17:14:26 +0200

    Do not complain about PropTypes

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
     "import/order": [
       "error",
       {
